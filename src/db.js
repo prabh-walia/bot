57d40c2b20d3d3e8851153526801de48384e568e
prabh-walia <prabhdeepwalia999@gmail.com>
2024-09-14 19:27:35 +0530

    db cloud changes

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose, { connect } from 'mongoose';
 
-connect('mongodb://localhost:27017/trade_base', { useNewUrlParser: true, useUnifiedTopology: true })
+connect('mongodb+srv://Prabhdeep:prabhdeep1@cluster0.lbwiw.mongodb.net/trade_base?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
