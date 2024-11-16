8ad8de197eec242f4d1010cf0c9d8d518987dc65
prabh-walia <prabhdeepwalia999@gmail.com>
2024-11-16 13:30:19 +0530

    time frame change

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const API_KEY = "xmxPbqL8kT5fPtD2MLl4Uy2OavWFV8efJKkKQiesiMCScVngccAkPSlQRwrKmGNZ";
 export const SECRET_KEY = "HVkZDSPjiA37Cf5L3URKSw4Aw53wfTDCONbSb8AJzHTJePxkX5Lq5zRDjmTP3ZAL";
 export const SYMBOL = 'BTC/USDT';
-export  let TIMEFRAME = '2h';
+export  let TIMEFRAME = '30m';
 export const LIMIT = 200;
 export const LEVERAGE =2;
 export const Ratio = 3
 export const AMOUNT =30
-export const BIGGER_TIMEFRAME = '4h'
+export const BIGGER_TIMEFRAME = '1h'
 export const FIXED_RISK_AMOUNT = 0.04
 
 
