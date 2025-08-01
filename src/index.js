9ddc80024649e0ee71dd33c38fad255a99467965
prabh-walia <prabhdeepwalia999@gmail.com>
2025-08-01 16:30:21 +0530

    .

--- src/index.js
+++ src/index.js
@@ -161,12 +161,12 @@ const findTrades = async () => {
       //s console.log("pivots for today -", pivots);
       const percentDiff = ((avg - smallEmat) / smallEmat) * 100;
 
-      if (percentDiff >= 7.5) {
+      if (percentDiff >= 5.5) {
         console.log(
           `🔻 Close is ${percentDiff.toFixed(2)}% above EMA — Bearish reversal`
         );
         trend = "bearish";
-      } else if (percentDiff <= -6.5) {
+      } else if (percentDiff <= -5) {
         console.log(
           `🔺 Close is ${Math.abs(percentDiff).toFixed(
             2
